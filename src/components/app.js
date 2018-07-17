2a00aaad6ae1c434cf88a1d769fe7070055329d0
Ana Lisnic <ana.lisnnic@gmail.com>
2018-07-17 19:20:14 +0300

    placed dark skews and refactored scss

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,10 @@ export default class App extends Component {
     return (
       <div className="grid">
         <h1>Birthday Countdown</h1>
+        <div className="grid__skew-dark-one-box"></div>
+        <div className="grid__skew-dark-two"></div>
+        <div className="grid__skew-dark-three"></div>
+
         <div className="grid__skew-light-one"></div>
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
